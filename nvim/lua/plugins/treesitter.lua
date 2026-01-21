40d7e294e5ce195a80f92cc7fbfcd6f228286f87
Koaps <koaps@users.noreply.github.com>
2026-01-20 23:23:45 -0800

    Sync main
    Files:
      - nvim/lua/plugins/treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             -- import nvim-treesitter plugin
-            local treesitter = require("nvim-treesitter.configs")
+            local treesitter = require("nvim-treesitter.config")
 
             -- configure treesitter
             treesitter.setup({ -- enable syntax highlighting
