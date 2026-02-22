104d6d9ff8174bd9a24fd60d9a0d41d47213f7f5
Koaps <koaps@users.noreply.github.com>
2026-02-21 22:25:39 -0800

    Sync main
    Files:
      - pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -2,6 +2,10 @@
 
 PYVER=3.14.2
 
+sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 if [ ! -d "$HOME/.pyenv" ]; then
   curl https://pyenv.run | bash
 fi
