a47cc6dc810d1820f52a61f171d9dcd06748237f
Koaps <koaps@users.noreply.github.com>
2026-02-28 10:27:36 -0800

    Sync main
    Files:
      - nvim/lua/plugins.lua
      - nvim/lua/statusline.lua
      - nvim/lua/terminal.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,7 +5,7 @@ vim.pack.add({
 })
 
 local function packadd(name)
-	vim.cmd("packadd " .. name)
+  vim.cmd("packadd " .. name)
 end
 
 packadd("vague.nvim")
@@ -15,22 +15,22 @@ packadd("fzf-lua")
 require("fzf-lua").setup({})
 
 vim.keymap.set("n", "<leader>ff", function()
-	require("fzf-lua").files()
+  require("fzf-lua").files()
 end, { desc = "FZF Files" })
 vim.keymap.set("n", "<leader>fg", function()
-	require("fzf-lua").live_grep()
+  require("fzf-lua").live_grep()
 end, { desc = "FZF Live Grep" })
 vim.keymap.set("n", "<leader>fb", function()
-	require("fzf-lua").buffers()
+  require("fzf-lua").buffers()
 end, { desc = "FZF Buffers" })
 vim.keymap.set("n", "<leader>fh", function()
-	require("fzf-lua").help_tags()
+  require("fzf-lua").help_tags()
 end, { desc = "FZF Help Tags" })
 vim.keymap.set("n", "<leader>fx", function()
-	require("fzf-lua").diagnostics_document()
+  require("fzf-lua").diagnostics_document()
 end, { desc = "FZF Diagnostics Document" })
 vim.keymap.set("n", "<leader>fX", function()
-	require("fzf-lua").diagnostics_workspace()
+  require("fzf-lua").diagnostics_workspace()
 end, { desc = "FZF Diagnostics Workspace" })
 
 require("mini.ai").setup({})
@@ -47,51 +47,51 @@ require("mini.icons").setup({})
 
 local miniclue = require("mini.clue")
 miniclue.setup({
-	triggers = {
-		-- Leader triggers
-		{ mode = "n", keys = "<Leader>" },
-		{ mode = "x", keys = "<Leader>" },
+  triggers = {
+    -- Leader triggers
+    { mode = "n", keys = "<Leader>" },
+    { mode = "x", keys = "<Leader>" },
 
-		-- Built-in completion
-		{ mode = "i", keys = "<C-x>" },
+    -- Built-in completion
+    { mode = "i", keys = "<C-x>" },
 
-		-- `g` key
-		{ mode = "n", keys = "g" },
-		{ mode = "x", keys = "g" },
+    -- `g` key
+    { mode = "n", keys = "g" },
+    { mode = "x", keys = "g" },
 
-		-- Marks
-		{ mode = "n", keys = "'" },
-		{ mode = "n", keys = "`" },
-		{ mode = "x", keys = "'" },
-		{ mode = "x", keys = "`" },
+    -- Marks
+    { mode = "n", keys = "'" },
+    { mode = "n", keys = "`" },
+    { mode = "x", keys = "'" },
+    { mode = "x", keys = "`" },
 
-		-- Registers
-		{ mode = "n", keys = '"' },
-		{ mode = "x", keys = '"' },
-		{ mode = "i", keys = "<C-r>" },
-		{ mode = "c", keys = "<C-r>" },
+    -- Registers
+    { mode = "n", keys = '"' },
+    { mode = "x", keys = '"' },
+    { mode = "i", keys = "<C-r>" },
+    { mode = "c", keys = "<C-r>" },
 
-		-- Window commands
-		{ mode = "n", keys = "<C-w>" },
+    -- Window commands
+    { mode = "n", keys = "<C-w>" },
 
-		-- `z` key
-		{ mode = "n", keys = "z" },
-		{ mode = "x", keys = "z" },
+    -- `z` key
+    { mode = "n", keys = "z" },
+    { mode = "x", keys = "z" },
 
-		-- Bracketed
-		{ mode = "n", keys = "[" },
-		{ mode = "n", keys = "]" },
-	},
+    -- Bracketed
+    { mode = "n", keys = "[" },
+    { mode = "n", keys = "]" },
+  },
 
-	clues = {
-		-- Enhance this by adding descriptions for <Leader> mapping groups
-		miniclue.gen_clues.builtin_completion(),
-		miniclue.gen_clues.g(),
-		miniclue.gen_clues.marks(),
-		miniclue.gen_clues.registers(),
-		miniclue.gen_clues.windows(),
-		miniclue.gen_clues.z(),
-	},
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
 })
 
 vim.cmd("colorscheme vague")
